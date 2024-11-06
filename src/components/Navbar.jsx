93f9d07a9717ef472ae08dbdeee68a02ac880949
rahulap-aot <rahul.ap@aot-technologies.com>
2024-11-06 00:53:06 -0800

    check

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,44 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
-import { styles } from '../styles'
-import { navLinks } from '../constants'
-import { logo, menu, close } from '../assets'
+import { styles } from '../styles';
+import { navLinks } from '../constants';
+import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
-  const [active, setActive] = useState('')
+  const [active, setActive] = useState('');
 
   return (
     <nav>
       <div className={`${styles.paddingX} w-full items-center py-5 fixed top-0 z-20 bg-primary`}>
         <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-          <Link to="/"
-          className="flex items-center gap-2"
-          onClick={() => {
-            setActive("");
-            window.scrollTo(0, 0);
-            }}>
-            <img src={logo} alt="Logo" className='w-`9 h-9 object-contain'/>
-            <p className='text-white text-[18px] font-bold cursor-pointer'>Rahul <span className='sm:block hidden'>A P</span></p>
+          <Link
+            to="/"
+            className="flex items-center gap-2"
+            onClick={() => {
+              setActive('');
+              window.scrollTo(0, 0);
+            }}
+          >
+            <img src={logo} alt="Logo" className="w-9 h-9 object-contain" />
+            <p className="text-white text-[18px] font-bold cursor-pointer">
+              Rahul <span className="sm:block hidden">A P</span>
+            </p>
           </Link>
-          </div>
+
+          <ul className="list-none hidden sm:flex flex-row gap-10">
+            {navLinks.map((link) => (
+              <li key={link.id}>
+                <a href={`#${link.id}`} className="text-white text-[18px] font-medium cursor-pointer hover:text-secondary">
+                  {link.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
